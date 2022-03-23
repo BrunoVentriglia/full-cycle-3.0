c5b2df9b923b19daeb784ec8958d6d7e4abd5d06
Bruno <brunovtr@gmail.com>
2022-03-23 18:58:51 -0300

    Ajuste

--- docker/desafio-node/index.js
+++ docker/desafio-node/index.js
@@ -5,23 +5,24 @@ const app = express();
 app.get('/', function (req, res) {
     peopleService.ListAll(function (listPeopleName) {
 
-        peopleService.Insert();
-        
-        res.send(`<h1>Full Cycle Rocks!</h1><h3>Lista de nomes</h3><ul>${listPeopleName}</ul>`);
+        setTimeout(function() {
+            res.send(`<h1>Full Cycle Rocks!</h1><h3>Lista de nomes</h3><ul>${listPeopleName}</ul>`);
+        }, 1000)
+
         //res.send(`<h1>Full Cycle Rocks!</h1><h3>Lista de nomes</h3>`);
     });
 
 });
 
 app.listen(3000, function () {
-    console.log('NGINX rodando na porta 8080');
-    sleep(2000);
-    peopleService.CreateDb();
-    sleep(2000);
-});
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
+    console.log('NGINX rodando na porta 8080');
+
+    //setTimeout(function() {
+        peopleService.CreateDb();
+    //}, 5000)
+
+    setTimeout(function() {
+        peopleService.Insert();
+    }, 1000)
+});
