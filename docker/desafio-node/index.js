7550a7488b0994c3cece629854385622f24c8b40
Bruno <brunovtr@gmail.com>
2022-03-23 18:15:58 -0300

    Ajuste

--- docker/desafio-node/index.js
+++ docker/desafio-node/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const peopleService = require('./service');
 const app = express();
 
-peopleService.Insert();
-
 app.get('/', function (req, res) {
     peopleService.ListAll(function (listPeopleName) {
+
+        peopleService.Insert();
+        
         res.send(`<h1>Full Cycle Rocks!</h1><h3>Lista de nomes</h3><ul>${listPeopleName}</ul>`);
         //res.send(`<h1>Full Cycle Rocks!</h1><h3>Lista de nomes</h3>`);
     });
