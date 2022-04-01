7b64eb9cba1bf5abd98ca51022fd68ae050326cb
Bruno <brunovtr@gmail.com>
2022-04-01 16:22:11 -0300

    Ajustes

--- docker/desafio-node/index.js
+++ docker/desafio-node/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const peopleService = require('./service');
 const app = express();
 
-peopleService.CreateDb();
+setTimeout(() => {
+    peopleService.CreateDb();
+}, 3000);
 
 setTimeout(() => {
     peopleService.Insert();
-}, 2000);
+}, 5000);
 
 app.get('/', (req, res) => {
     peopleService.ListAll((listOfNames) => {
